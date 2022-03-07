dc478704ab5bfa3a5b7afe434b209d78e40d3645
Marcos Curtidor <3048652220116@ingenieria.usac.edu.gt>
2022-03-07 16:23:10 -0600

    Implementacion load balancer
    Se agregaron las peticiones hacia el load balancer y se distribuyo todos los datos segun corresponda a cada maquina virtual

--- Fase1/frontend/src/pages/CpuPage/CpuPage.js
+++ Fase1/frontend/src/pages/CpuPage/CpuPage.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./CpuPage.css";
 import NavBar from "../../components/NavBar/NavBar";
-import Tree from '@naisutech/react-tree'
+import Tree from '@naisutech/react-tree';
+import Swal from 'sweetalert2';
 
 export default function CpuPage() {
 
@@ -9,132 +10,223 @@ export default function CpuPage() {
     allData: []
   })
 
+  const [proc2, setProc2] = useState({
+    allData: []
+  })
+
   function getData() {
-    fetch(`http://${process.env.REACT_APP_IPAPI}/getCPUstatus`)
+    fetch(`http://${process.env.REACT_APP_IPAPI}:80/getCPUstatus`)
       .then(res => res.json())
       .then(data => {
-        data.forEach(value => {
-          let myNode = []
-          let id = value.pid
-          let parentId = value.ppid
-          let nameP = value.name
-          let state = value.state
-          let childs = value.childs
-          let newParentId = null
+        let values = data.data
+        if (data.vm === 1) {
+          values.forEach(value => {
+            let myNode = []
+            let id = value.pid
+            let parentId = value.ppid
+            let nameP = value.name
+            let state = value.state
+            let childs = value.childs
+            let newParentId = null
 
-          if (parentId != 0){
-            newParentId = parentId
-          }
+            if (parentId !== 0) {
+              newParentId = parentId
+            }
 
-          switch (state) {
-            case 0:
-              state = 'Running'
-              break
-            case 1:
-              state = 'Interrumpible Sleeping'
-              break
-            case 2:
-              state = 'Uninterrumpible Sleeping'
-              break
-            case 1026 :
-              state = 'Idle'
-              break
-            default:
-              state = '0'
-              break
-          }
-
-          let newFormat = {
-            "id": id,
-            "parentId": newParentId,
-            "label": `Proceso: ${nameP}`,
-            "items": [{
-              "label": `PID: ${id}`,
-              "parentId": id
-            }, {
-              "label": `PID Padre: ${parentId}`,
-              "parentId": id
-            }, {
-              "label": `Estado: ${state}`,
-              "parentId": id
-            } ]
-          }
-          myNode.push(newFormat)
-
-          childs.forEach(child => {
-            let nameChild = child.name
-            let ppidChild = child.ppid
-            let pidChild = child.pid
-            let stateChild = child.state
-
-            switch (stateChild) {
+            switch (state) {
               case 0:
-                stateChild = 'Running'
+                state = 'Running'
                 break
               case 1:
-                stateChild = 'Interrumpible Sleeping'
+                state = 'Interrumpible Sleeping'
                 break
               case 2:
-                stateChild = 'Uninterrumpible Sleeping'
+                state = 'Uninterrumpible Sleeping'
                 break
-              case 1026 :
-                stateChild = 'Idle'
+              case 1026:
+                state = 'Idle'
                 break
               default:
-                stateChild = '0'
+                state = '0'
                 break
             }
 
-            let newFormatChild = {
-              "id": pidChild,
-              "parentId": ppidChild,
-              "label": `Proceso: ${nameChild}`,
+            let newFormat = {
+              "id": id,
+              "parentId": newParentId,
+              "label": `Proceso: ${nameP}`,
               "items": [{
-                "label": `PID: ${pidChild}`,
-                "parentId": pidChild
+                "label": `PID: ${id}`,
+                "parentId": id
               }, {
-                "label": `PID Padre: ${ppidChild}`,
-                "parentId": pidChild
+                "label": `PID Padre: ${parentId}`,
+                "parentId": id
               }, {
-                "label": `Estado: ${stateChild}`,
-                "parentId": pidChild
-              } ]
+                "label": `Estado: ${state}`,
+                "parentId": id
+              }]
             }
-            myNode.push(newFormatChild)
-          })
+            myNode.push(newFormat)
 
-          proc1.allData.push(myNode)
+            childs.forEach(child => {
+              let nameChild = child.name
+              let ppidChild = child.ppid
+              let pidChild = child.pid
+              let stateChild = child.state
 
-          setProc1({
-            allData: proc1.allData
-          })
-        });
+              switch (stateChild) {
+                case 0:
+                  stateChild = 'Running'
+                  break
+                case 1:
+                  stateChild = 'Interrumpible Sleeping'
+                  break
+                case 2:
+                  stateChild = 'Uninterrumpible Sleeping'
+                  break
+                case 1026:
+                  stateChild = 'Idle'
+                  break
+                default:
+                  stateChild = '0'
+                  break
+              }
+
+              let newFormatChild = {
+                "id": pidChild,
+                "parentId": ppidChild,
+                "label": `Proceso: ${nameChild}`,
+                "items": [{
+                  "label": `PID: ${pidChild}`,
+                  "parentId": pidChild
+                }, {
+                  "label": `PID Padre: ${ppidChild}`,
+                  "parentId": pidChild
+                }, {
+                  "label": `Estado: ${stateChild}`,
+                  "parentId": pidChild
+                }]
+              }
+              myNode.push(newFormatChild)
+            })
+
+            proc1.allData.push(myNode)
+
+            setProc1({
+              allData: proc1.allData
+            })
+          });
+        } else if (data.vm === 2){
+          values.forEach(value => {
+            let myNode = []
+            let id = value.pid
+            let parentId = value.ppid
+            let nameP = value.name
+            let state = value.state
+            let childs = value.childs
+            let newParentId = null
+
+            if (parentId !== 0) {
+              newParentId = parentId
+            }
+
+            switch (state) {
+              case 0:
+                state = 'Running'
+                break
+              case 1:
+                state = 'Interrumpible Sleeping'
+                break
+              case 2:
+                state = 'Uninterrumpible Sleeping'
+                break
+              case 1026:
+                state = 'Idle'
+                break
+              default:
+                state = '0'
+                break
+            }
+
+            let newFormat = {
+              "id": id,
+              "parentId": newParentId,
+              "label": `Proceso: ${nameP}`,
+              "items": [{
+                "label": `PID: ${id}`,
+                "parentId": id
+              }, {
+                "label": `PID Padre: ${parentId}`,
+                "parentId": id
+              }, {
+                "label": `Estado: ${state}`,
+                "parentId": id
+              }]
+            }
+            myNode.push(newFormat)
+
+            childs.forEach(child => {
+              let nameChild = child.name
+              let ppidChild = child.ppid
+              let pidChild = child.pid
+              let stateChild = child.state
+
+              switch (stateChild) {
+                case 0:
+                  stateChild = 'Running'
+                  break
+                case 1:
+                  stateChild = 'Interrumpible Sleeping'
+                  break
+                case 2:
+                  stateChild = 'Uninterrumpible Sleeping'
+                  break
+                case 1026:
+                  stateChild = 'Idle'
+                  break
+                default:
+                  stateChild = '0'
+                  break
+              }
+
+              let newFormatChild = {
+                "id": pidChild,
+                "parentId": ppidChild,
+                "label": `Proceso: ${nameChild}`,
+                "items": [{
+                  "label": `PID: ${pidChild}`,
+                  "parentId": pidChild
+                }, {
+                  "label": `PID Padre: ${ppidChild}`,
+                  "parentId": pidChild
+                }, {
+                  "label": `Estado: ${stateChild}`,
+                  "parentId": pidChild
+                }]
+              }
+              myNode.push(newFormatChild)
+            })
+
+            proc2.allData.push(myNode)
+
+            setProc2({
+              allData: proc2.allData
+            })
+          });
+        }
+
+        
+        Swal.fire({
+          title: `Procesos cargados de VM${data.vm}`,
+          icon: 'success',
+          confirmButtonText: 'Cerrar'
+        })
       })
   }
 
   useEffect(() => {
     getData()
   }, [])
-
-  const data = [
-    {
-      "id": 12345678,
-      "parentId": null,
-      "label": "Proceso1, PID: ... , PID Padre: ..., Estado: ....",
-      "items": []
-    },
-    {
-      "id": 56789012,
-      "parentId": 12345678,
-      "label": "My child node",
-      "items": []
-    },
-    {
-      "id": 87654321,
-      "label": "My file",
-      "parentId": 12345678
-    }
-  ]
 
   return (
     <div id="body">
@@ -145,7 +237,7 @@ export default function CpuPage() {
         <p className="fs-2 text-start text-white">Procesos</p>
         {
           proc1.allData.map(nodo => {
-            return(
+            return (
               <Tree nodes={nodo} theme="light" />
             )
           })
@@ -156,7 +248,14 @@ export default function CpuPage() {
       <div id="vm2" className="bg-success bg-gradient border border-dark">
         <p className="fs-1 text-center">VM2</p>
         <p className="fs-2 text-start text-white">Procesos</p>
-        <Tree nodes={data} theme="light" />
+        
+        {
+          proc2.allData.map(nodo => {
+            return (
+              <Tree nodes={nodo} theme="light" />
+            )
+          })
+        }
       </div>
     </div>
   )
