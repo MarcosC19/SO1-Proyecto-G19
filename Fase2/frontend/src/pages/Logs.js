c711bc0a0e159019d0b889be06687f182b336443
Steven Jocol <steven.jsg@gmail.com>
2022-04-06 21:22:11 -0600

    Logs Report
    Implemente los reportes de los Logs en el front

--- Fase2/frontend/src/pages/Logs.js
+++ Fase2/frontend/src/pages/Logs.js
@@ -2,17 +2,24 @@ import React,{useRef, useState, useEffect} from 'react';
 import '../css/Logs.css';
 import DataTable from 'react-data-table-component';
 import NavBar from '../components/NavBar';
+import CanvasJSReact from './../components/Canvas/canvasjs.react';
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 const RUST_API_HOST = "https://rust-api-4waihun6ya-uc.a.run.app";
 
 const columns = [
     {
         name: 'Nombre del Juego',
-        selector: row => row.game_name,
+        selector: row => row.game_name===""?("Piedra, Papel o Tijeras"):row.game_name,
     },
     {
         name: 'Ganador',
         selector: row => row.winner,
+    },
+    {
+        name: "Jugadores",
+        selector: row => row.players,
+        
     },
     {
         name: 'Ruta',
@@ -20,16 +27,118 @@ const columns = [
     },
 ];
 
+const paginationOptions = {
+	rowsPerPageText: 'Filas por página',
+	rangeSeparatorText: 'de',
+	selectAllRowsItem: true,
+	selectAllRowsItemText: 'Todos',
+};
+
+
 export default function Logs(){
 
     const [tableData, setTableData] = useState([]);
+    const [char1Data, setChar1Data] = useState([]);
+    const [char2Data, setChar2Data] = useState([]);
+
+    const options_top = {
+        animationEnabled: true,
+        theme: "light2",
+        title: {
+            text: "Top 3 Games"
+        },
+        axisY: {
+            title: "Partidas Jugadas",
+        },
+        axisX: {
+            title: "Juego"
+        },
+        data: [{
+            type: "bar",
+            dataPoints: char1Data
+        }]
+    };
+
+    const options_queue = {
+        animationEnabled: true,
+        theme: "light2",
+        title: {
+            text: "Inserciones por Queue"
+        },
+        axisY: {
+            title: "Inserciones",
+        },
+        axisX: {
+            title: "Plataforma"
+        },
+        data: [{
+            type: "bar",
+            dataPoints: char2Data
+        }]
+    };
 
     useEffect(() => {
-        console.log("Fetching Logs Data");
-        getLogs();
-
-
+        fillData();
+        let updateTask = setInterval(fillData, 1000);
+        
+        return () => {
+            clearInterval(updateTask);
+        }
     },[]);
+
+    const fillData = () => {
+        console.log("Fetching Logs Data");
+        getLogs().then((result) => {
+
+            // Set General Log Data Table Info
+            setTableData(result);
+
+
+            // Set Chart of Top 3
+            let topList = {};
+            result.forEach((game) => {
+                if(topList[game.game_name] !== undefined){
+                    topList[game.game_name] = topList[game.game_name] + 1;
+                }else{
+                    topList[game.game_name] = 1;
+                }
+            });
+
+            let dotList_top = [];
+            for(var key in topList){
+                let np = {
+                    y: parseInt(topList[key]), label: key
+                };
+                dotList_top.push(np);
+            };
+
+            dotList_top.sort();
+            dotList_top = dotList_top.slice(0,3);
+            setChar1Data(dotList_top);
+
+            // Set Chart of Number of Inserts per Queue
+
+            let insertList = {};
+            
+            result.forEach((game) => {
+                if(insertList[game.queue] !== undefined){
+                    insertList[game.queue] = insertList[game.queue] + 1;
+                }else{
+                    insertList[game.queue] = 1;
+                }
+            });
+
+            let dotList_queue = [];
+            for(var key in insertList){
+                let np = {
+                    y: parseInt(insertList[key]), label: key
+                };
+                dotList_queue.push(np);
+            };
+
+            setChar2Data(dotList_queue);
+        });
+    }
 
     const getLogs = async() => {
         const options = {
@@ -55,12 +164,20 @@ export default function Logs(){
     return(
         <div>
             <NavBar/>
-            <h1>Pagina Logs</h1>
+            <h1>Historial y Reporte de Juegos</h1>
 
             <DataTable
                 columns={columns}
                 data={tableData}
+                pagination
+                paginationComponentOptions={paginationOptions}
             />
+            <br/>
+            <hr/>
+            <CanvasJSChart options={options_top} />
+            <br/>
+            <hr/>
+            <CanvasJSChart options={options_queue} />
         </div>
     )
 }
