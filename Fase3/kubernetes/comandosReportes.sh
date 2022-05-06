5836136aa48c143f0604808b122a8ea56573cbff
Marcos Curtidor <3048652220116@ingenieria.usac.edu.gt>
2022-05-06 00:10:51 -0600

    Ingress controller y linkerd
    Se realizo la implementacion del ingress controller para los reportes y se inyecto linkerd en cada uno de los deployments

--- Fase3/kubernetes/comandosReportes.sh
+++ Fase3/kubernetes/comandosReportes.sh
@@ -1,24 +1,48 @@
 # CREACION DE KLUSTER K8S
 gcloud container clusters create proyecto-g19 --num-nodes=3 --tags=allin,allout --machine-type=n1-standard-2 --no-enable-network-policy
 
+# CREACION NAMESPACE INGRESS CONTROLLER
+kubectl create ns nginx-ingress
+
+# INSTALACION INGRESS CONTROLLER
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+
+helm repo update
+
+helm install nginx-ingress ingress-nginx/ingress-nginx -n nginx-ingress
+
+# INSTALACION LINKERD
+curl --proto '=https' --tlsv1.2 -sSfL https://run.linkerd.io/install | sh
+linkerd version
+linkerd install | kubectl apply -f -
+
+# INSTALACION DASHBOARD
+linkerd viz install | kubectl apply -f -
+linkerd viz dashboard
+
+# INYECCION LINKERD INGRESS CONTROLLER FRONTEND
+kubectl get -n nginx-ingress deploy nginx-ingress-ingress-nginx-controller -o yaml \
+| linkerd inject - \
+| kubectl apply -f -
+
 # CREACION NAMESPACE
-kubectl create namespace squidgame --dry-run=client -o yaml > configG19.yaml
+kubectl create namespace squidgame --dry-run=client -o yaml > configInit.yaml
 
-# CREACION POD SOCKET.IO
-kubectl run games-result --image=curtex19/games-result-f3 --restart=Never --namespace=squidgame --dry-run=client -o yaml >> configG19.yaml
+# CREACION DEPLOY SOCKET.IO
+kubectl create deploy games-result --image=curtex19/games-result-f3 --replicas=1 --namespace=squidgame --dry-run=client -o yaml >> configInit.yaml
 
-# CREACION CLUSTERIP SOCKET.IO
-kubectl expose pod/games-result --type=ClusterIP --port=80 --namespace=squidgame --dry-run=client -o yaml >> configG19.yaml
+# CREACION LOADBALANCER SOCKET.IO
+kubectl expose deploy/games-result --type=LoadBalancer --port=80 --namespace=squidgame --dry-run=client -o yaml >> configInit.yaml
 
-# CREACION POD LOGS
-kubectl run games-logs --image=curtex19/games-logs-f3 --restart=Never --namespace=squidgame --dry-run=client -o yaml >> configG19.yaml
+# CREACION DEPLOY LOGS
+kubectl create deploy games-logs --image=curtex19/games-logs-f3 --replicas=1 --namespace=squidgame --dry-run=client -o yaml >> configInit.yaml
 
-# CREACION CLUSTERIP LOGS
-kubectl expose pod/games-logs --type=ClusterIP --port=8080 --namespace=squidgame --dry-run=client -o yaml >> configG19.yaml
+# CREACION LOADBALANCER LOGS
+kubectl expose deploy/games-logs --type=LoadBalancer --port=8080 --namespace=squidgame --dry-run=client -o yaml >> configInit.yaml
 
-# CREACION POD FRONTEND
-kubectl run frontend --image=curtex19/frontend-f3 --restart=Never --namespace=squidgame --dry-run=client -o yaml >> configG19.yaml
+# CREACION DEPLOY FRONTEND
+kubectl create deploy frontend --image=curtex19/frontend-f3 --replicas=1 --namespace=squidgame --dry-run=client -o yaml >> configReports.yaml
 
 # CREACION CLUSTERIP FRONTEND
-kubectl expose pod/frontend --type=ClusterIP --port=80 --namespace=squidgame --dry-run=client -o yaml >> configG19.yaml
+kubectl expose deploy/frontend --type=ClusterIP --port=3000 --namespace=squidgame --dry-run=client -o yaml >> configReports.yaml
 
